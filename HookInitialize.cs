765894d7d44ec89234faf400bee07bdaff28e818
josh1351 <joshua.rios@authentix.com>
2018-02-12 10:51:27 -0600

    Add files via upload

---
+++ HookInitialize.cs
@@ -0,0 +1,27 @@
+﻿using EAAutoFramework.Base;
+using TechTalk.SpecFlow;
+using EAAutoFramework.Helpers;
+using EAAutoFramework.Config;
+
+namespace EAEmployeeTest.Hooks
+{
+
+    [Binding]
+    public class HookInitialize : TestInitializeHook
+    {
+
+        [BeforeFeature]
+        public static void TestStart()
+        {
+            InitializeSettings();
+            Settings.ApplicationCon = Settings.ApplicationCon.DBConnect(Settings.AppConnectionString);
+        }
+
+        [AfterScenario]
+        public void TestStop()
+        {
+            DriverContext.Driver.Quit();
+        }
+
+    }
+}
