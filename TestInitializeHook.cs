fe93812de5e69480653e8798b19a122477dd24fe
josh1351 <joshua.rios@authentix.com>
2018-07-16 14:03:11 -0500

    Add files via upload

---
+++ TestInitializeHook.cs
@@ -0,0 +1,58 @@
+﻿using EAAutoFramework.Config;
+using EAAutoFramework.Helpers;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace EAAutoFramework.Base
+{
+    public abstract class TestInitializeHook : Base
+    {
+        public static void InitializeSettings()
+        {
+            //Set all the settings for framework
+            ConfigReader.SetFrameworkSettings();
+
+            //Set Log
+            LogHelpers.CreateLogFile();
+
+            //Open Browser
+            OpenBrowser(Settings.BrowserType);
+
+            LogHelpers.Write("Initialized framework");
+
+        }
+
+        private static void OpenBrowser(BrowserType browserType = BrowserType.FireFox)
+        {
+            switch (browserType)
+            {
+                case BrowserType.InternetExplorer:
+                    DriverContext.Driver = new InternetExplorerDriver();
+                    DriverContext.Browser = new Browser(DriverContext.Driver);
+                    break;
+                case BrowserType.FireFox:
+                    var binary = new FirefoxBinary(@"C:\Program Files (x86)\Mozilla Firefox\firefox.exe");
+                    var profile = new FirefoxProfile();
+                    DriverContext.Driver = new FirefoxDriver(binary, profile);
+                    DriverContext.Browser = new Browser(DriverContext.Driver);
+                    break;
+                case BrowserType.Chrome:
+                    DriverContext.Driver = new ChromeDriver();
+                    DriverContext.Browser = new Browser(DriverContext.Driver);
+                    break;
+            }
+
+        }
+
+        public virtual void NaviateSite()
+        {
+            DriverContext.Browser.GoToUrl(Settings.AUT);
+            LogHelpers.Write("Opened the browser !!!");
+        }
+
+
+
+    }
+}
